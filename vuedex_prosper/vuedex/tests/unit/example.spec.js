43fcb23f37075a3f254929a63bd56f0605d87805
klvin23 <klvin.g23@gmail.com>
2022-08-16 09:49:23 -0500

    Pruebas unitarias

---
+++ vuedex_prosper/vuedex/tests/unit/example.spec.js
@@ -0,0 +1,20 @@
+import {huevo, espinaca, arroz} from  "./jestTest";
+
+describe('Se va a probar la calidad de los ingredientes',() => {
+
+  test('Calidad del huevo', ()  => {
+    expect(huevo().estado[0]).toBe('fresco')
+  });
+
+  test('Calidad de la espinaca', ()  => {
+    expect(espinaca()).toEqual({
+      estado: 1,
+      color: "verde"
+    })
+  });
+
+  test('Calidad del arroz', () => {
+    expect(arroz().estado[0]).toBe('cocinado')
+  });
+
+});
---
+++ vuedex_prosper/vuedex/tests/unit/jestTest.js
@@ -0,0 +1,25 @@
+function huevo() {
+    return {
+        estado: ['fresco'],
+        color: '#000'
+    }
+}
+
+function espinaca() {
+    return {
+        estado: 1,
+        color: "verde"
+    }
+}
+
+function arroz() {
+    return {
+        estado: ['cocinado']
+    }
+}
+
+module.exports = {
+    huevo,
+    espinaca,
+    arroz
+};
